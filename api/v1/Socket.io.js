9b37ec99594555e3046fd94da787723961f8c7df
omardoma <omar.elias.96@gmail.com>
2018-05-25 01:18:50 +0200

    Improvements

--- api/v1/Socket.io.js
+++ api/v1/Socket.io.js
@@ -6,29 +6,31 @@ io.adapter(redisAdapter({ host: config.REDIS_HOST, port: config.REDIS_PORT }));
 
 io.on('connection', (socket) => {
   socket.on('join', (data) => {
-    socket.join(data.room, () => {
-      socket.to(data.room).emit('join', data);
+    socket.join(data.room, (err) => {
+      if (!err) {
+        socket.to(data.room).emit('join', data);
+      }
     });
   });
 
-  socket.on('notification', (notification) => {
+  socket.on('notification', (data) => {
     io.of('/').adapter.clientRooms(socket.id, (err, rooms) => {
       if (!err) {
         rooms.forEach((room) => {
-          socket.to(room).emit('notification', notification);
+          socket.to(room).emit('notification', data);
         });
       }
     });
   });
 
-  socket.on('message', (msg) => {
-    io.of('/').adapter.clientRooms(socket.id, (err, rooms) => {
+  socket.on('message', (data) => {
+    socket.to(data.room).emit('message', data);
+  });
+
+  socket.on('leave', (data) => {
+    socket.leave(data.room, (err) => {
       if (!err) {
-        rooms.forEach((room) => {
-          if (room === msg.room) {
-            socket.to(room).emit('message', msg);
-          }
-        });
+        socket.to(data.room).emit('leave', data);
       }
     });
   });
